a19b03177dad06b91b8d4f360314f825b2447893
Marco <francisco_dl24@hotmail.com>
2022-05-21 00:11:02 +0000

    primer commit sauip5

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["Ingresos/registros/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"Ingresos/registros/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
